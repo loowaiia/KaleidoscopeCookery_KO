a9a70166871cf4cac3537250e74c16d78553e89c
tartaric_acid <baka943@qq.com>
2025-07-13 19:05:55 +0800

    完成 crop 部分的重构

--- src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/block/crop/ChiliCropBlock.java
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/block/crop/ChiliCropBlock.java
@@ -2,8 +2,6 @@ package com.github.ysbbbbbb.kaleidoscopecookery.block.crop;
 
 import com.github.ysbbbbbb.kaleidoscopecookery.init.ModItems;
 import net.minecraft.core.BlockPos;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
@@ -18,17 +16,15 @@ public class ChiliCropBlock extends BaseCropBlock {
     }
 
     @Override
-    public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
-        if (pState.getValue(AGE) >= this.getMaxAge()) {
-            if (!pLevel.isClientSide) {
-                Block.popResource(pLevel, pPos, this.result.get().getDefaultInstance());
-                // 20% 几率掉落青椒
-                if (pLevel.getRandom().nextInt(5) == 0) {
-                    Block.popResource(pLevel, pPos, ModItems.GREEN_CHILI.get().getDefaultInstance());
-                }
-                pLevel.playSound(null, pPos.getX() + 0.5, pPos.getY() + 0.5, pPos.getZ() + 0.5, SoundEvents.CROP_BREAK, SoundSource.BLOCKS, 1.0F, 1.0F);
-                pLevel.setBlock(pPos, this.getStateForAge(5), Block.UPDATE_CLIENTS);
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult) {
+        if (state.getValue(AGE) >= this.getMaxAge()) {
+            // 20% 几率掉落青椒
+            if (level.getRandom().nextInt(5) == 0) {
+                Block.popResource(level, pos, ModItems.GREEN_CHILI.get().getDefaultInstance());
+            } else {
+                Block.popResource(level, pos, this.result.get().getDefaultInstance());
             }
+            super.onUseBreakCrop(level, pos);
             return InteractionResult.SUCCESS;
         }
         return InteractionResult.PASS;
